bd0c1fd7856b2da0e603a3f3e4c0594409cb5b6d
Casey Marshall <csm@noonhome.com>
2020-01-16 11:29:39 -0800

    Handle edn template strings properly.
    * index.js (edn): new function, properly read template strings. Update edn export.

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 'use strict';
 
 let jsedn = require('jsedn');
-module.exports = Object.assign({edn: (s) => jsedn.parse(s[0])}, require('./src/client.js'), require('./src/query.js'), require('./src/tx.js'));
+
+function edn(s, ...args) {
+    let ednStr = '';
+    for (let i = 0; i < s.length || i < args.length; i++) {
+        if (i < s.length) {
+            ednStr += s[i];
+        }
+        if (i < args.length) {
+            ednStr += args[i];
+        }
+    }
+    return jsedn.parse(ednStr);
+}
+
+module.exports = Object.assign({edn: edn}, require('./src/client.js'), require('./src/query.js'), require('./src/tx.js'));
